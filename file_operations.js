e763282947de8eb26e4eaa8b38b0e97430646b7a
Peter Marincheshki <pamarincheshki@gmail.com>
2024-07-24 20:39:33 +0300

    Moving images to a specific Images Folder

--- file_operations.js
+++ file_operations.js
@@ -3,6 +3,8 @@ const path = require('path');
 const{ createFolder } = require("./folder_operations");
 
 async function getFiles(directoryPath) {
+    const imageExtensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'webp', 'heif', 'svg'];
+
     try {
         const files = await fs.readdir(directoryPath);
         for (const file of files) {
@@ -16,13 +18,21 @@ async function getFiles(directoryPath) {
                 }
                 // File is NOT a folder - Getting the extension
 
-                const ext = path.extname(file);
+                let ext = path.extname(file);
+                let newFolderPath= "";
                 if(ext) {
-                    const folderName = ext.slice(1);
-                    const newFolderPath = await createFolder(directoryPath, folderName);
+                    if(imageExtensions.includes(ext.slice(1))) {
+                        newFolderPath = path.join(directoryPath, "Images");
+                    } else {
+                      ext = ext.toUpperCase();
+                      newFolderPath = path.join(directoryPath, `${ext} Files`);
+                    }
+
+                    await createFolder(newFolderPath);
                     const movedFilePath =  path.join(newFolderPath, file);
                     await fs.rename(filePath, movedFilePath);
                     console.log(`File moved to ${movedFilePath}`);
+                
 
                 }
             } catch (err){
--- folder_operations.js
+++ folder_operations.js
@@ -1,22 +1,33 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-async function createFolder(folderPath, extension) {
-    const folderName = `${extension}`.toUpperCase();
-    const newFolderPath = path.join(folderPath, `${folderName} Files`);
-
+async function createFolder(newFolderPath) {
     // Try to create the new Folder path
     try {
         await fs.mkdir(newFolderPath);
         console.log(`Folder Created! \n${newFolderPath}`);
+   
     } catch (error) {
-        if (err.code !== 'EEXIST') {
-            throw err;
-        } else {
+        // If exists go to the next file without throwing an error
+        // otherwise - throws an error
+        if (error.code === 'EEXIST') {
             console.log(`Folder already exists: ${newFolderPath}`);
+        } else if (error.code === 'EACCES') {
+            console.error(`Permission denied: Unable to create folder at ${newFolderPath}`);
+            throw error;
+        } else if (error.code === 'EINVAL') {
+            console.error(`Invalid path: ${newFolderPath}`);
+            throw error;
+        } else if (error.code === 'ENOENT') {
+            console.error(`No such file or directory: Unable to create folder at ${newFolderPath}`);
+            throw error;
+        } else if (error.code === 'EDQUOT') {
+            console.error(`Disk quota exceeded: Unable to create folder at ${newFolderPath}`);
+            throw error;
+        } else {
+            console.error(`An unexpected error occurred: ${error.message}`);
+            throw error;
         }
-        
     }
-    return newFolderPath;
 }
 module.exports = { createFolder };
